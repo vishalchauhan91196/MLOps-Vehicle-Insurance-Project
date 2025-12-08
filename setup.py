7e11a294d270e643993f1c221ed7e01fa064ca26
vishalchauhan91196 <vishalchauhan91196@gmail.com>
2025-12-08 18:27:23 +0530

    Added basic folder structure of project - part 2

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vishal",
+    author_email="vishal@gmail.com",
+    packages=find_packages()
+)
