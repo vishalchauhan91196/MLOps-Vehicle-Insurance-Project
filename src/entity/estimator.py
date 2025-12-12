7c33eb95e473f7b1e9f8f79ef52786d64d2965c7
vishalchauhan91196 <vishalchauhan91196@gmail.com>
2025-12-12 21:00:30 +0530

    Added model evaluation component

---
+++ src/entity/estimator.py
@@ -0,0 +1,36 @@
+import sys
+
+import pandas as pd
+from pandas import DataFrame
+from sklearn.pipeline import Pipeline
+
+from src.exception import MyException
+from src.logger import logging
+
+
+class MyModel:
+    def __init__(self, preprocessing_object: Pipeline, trained_model_object: object):
+        
+        self.preprocessing_object = preprocessing_object
+        self.trained_model_object = trained_model_object
+
+    def predict(self, dataframe: pd.DataFrame) -> DataFrame:
+        """
+        Function accepts preprocessed inputs (with all custom transformations already applied),
+        applies scaling using preprocessing_object, and performs prediction on transformed features.
+        """
+        try:
+            logging.info("Starting prediction process.")
+
+            # Step 1: Apply scaling transformations using the pre-trained preprocessing object
+            transformed_feature = self.preprocessing_object.transform(dataframe)
+
+            # Step 2: Perform prediction using the trained model
+            logging.info("Using the trained model to get predictions")
+            predictions = self.trained_model_object.predict(transformed_feature)
+
+            return predictions
+
+        except Exception as e:
+            logging.error("Error occurred in predict method", exc_info=True)
+            raise MyException(e, sys) from e
